fd875db3751bb0c14d27f1bdc3c536735a9ccd3d
Utasi Tamas <utasi.tamas@adaca.hu>
2017-05-20 17:57:59 +0200

    Moved code into core sub-module in preparation to adding an spi module, where json serialization api will be added.

--- core/src/main/java/com/github/karsaig/approvalcrest/matcher/ApprovedFileMatcher.java
+++ core/src/main/java/com/github/karsaig/approvalcrest/matcher/ApprovedFileMatcher.java
@@ -3,11 +3,11 @@ package com.github.karsaig.approvalcrest.matcher;
 public interface ApprovedFileMatcher<T extends ApprovedFileMatcher<T>> {
 	
 	/**
+	 * <pre>
 	 * Concatenates a unique ID to the generated file name. Useful for test cases
 	 * containing multiple verifications.
-	 * <br></br>
 	 * Eg: With <b>"id1"</b> as input hash-approved.ext becomes hash-<b>id1</b>-approved.ext
-	 *
+	 * </pre>
 	 * @param uniqueId
 	 *          a {@link String} object, that uniquely identifies the file.
 	 * @return current instance
@@ -29,7 +29,7 @@ public interface ApprovedFileMatcher<T extends ApprovedFileMatcher<T>> {
 	 * Sets the file path to the given parameter.
 	 * Directory represented by the given path will be used instead of the default directory name (hash code).
 	 *
-	 * @param fileName
+	 * @param pathName
 	 *          a {@link String} object, which will be the file path.
 	 * @return current instance
 	 */
